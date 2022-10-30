9efe4ac63f9a93f2cc6bf30f703491aa3e9ee54b
Sam Bhandari <sambhandari2021@yahooe.com>
2022-10-30 12:18:45 -0400

    merge all final js and html

--- jsEstimate.js
+++ jsEstimate.js
@@ -1,1 +1,40 @@
-hvhchgcg
+let numofDays;
+let dailyBasicChrgAmt = 30;
+let surCharge = .30;
+let tollTag = 3.95;
+let gps = 4.95;
+let roadsideAst = 2.95;
+
+estBtn.addEventListener('click', calTotalAmt);
+
+function calTotalAmt() {
+    let numOfDays = Number(document.getElementById('numOfDays').value);
+    ///for car rental with just basic daily rate
+    let totalBasicChrgAmt = dailyBasicChrgAmt * numOfDays;
+    document.querySelector('output.carRental').innerText = (`$${totalBasicChrgAmt.toFixed(2)}`);
+    ///for total optional charge
+    let totalOptChrgAmt = Number(getOptCharge()) * numOfDays ;
+    document.querySelector('output.option').innerText = (`$${totalOptChrgAmt.toFixed(2)}`);
+    ///for under 25 surcharge
+    document.getElementById('yes').checked ? totalSurChargeAmt = totalBasicChrgAmt * surCharge : totalSurChargeAmt = 0;
+    document.querySelector('output.surcharge').innerText = (`$${totalSurChargeAmt.toFixed(2)}`);
+    ///for total due
+    let totalDue = totalBasicChrgAmt + totalOptChrgAmt + totalSurChargeAmt;
+    document.querySelector('output.total').innerText = (`$${totalDue.toFixed(2)}`);
+ 
+}
+
+function getOptCharge() {
+    let optCharge = 0;
+
+    if (document.getElementById('tollTag').checked) {
+        optCharge += tollTag;
+    }
+    if (document.getElementById('gps').checked) {
+        optCharge += gps;
+    }
+    if (document.getElementById('roadside').checked) {
+        optCharge += roadsideAst;
+    }
+    return optCharge;
+}
