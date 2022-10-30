d0847369b7ff8155b5693ad59f8179f59d99fb07
Sam Bhandari <sambhandari2021@yahooe.com>
2022-10-30 12:09:13 -0400

    refoctor the code

--- jsEstimate.js
+++ jsEstimate.js
@@ -2,29 +2,22 @@ let numofDays;
 let dailyBasicChrgAmt = 30;
 let surCharge = .30;
 let tollTag = 3.95;
-let gps = 4;
-let roadsideAst = 2;
-
+let gps = 4.95;
+let roadsideAst = 2.95;
 
 estBtn.addEventListener('click', calTotalAmt);
 
 function calTotalAmt() {
     let numOfDays = Number(document.getElementById('numOfDays').value);
-
     ///for car rental with just basic daily rate
     let totalBasicChrgAmt = dailyBasicChrgAmt * numOfDays;
     document.querySelector('output.carRental').innerText = (`$${totalBasicChrgAmt.toFixed(2)}`);
-
     ///for total optional charge
     let totalOptChrgAmt = Number(getOptCharge()) * numOfDays ;
     document.querySelector('output.option').innerText = (`$${totalOptChrgAmt.toFixed(2)}`);
-
-
     ///for under 25 surcharge
     document.getElementById('yes').checked ? totalSurChargeAmt = totalBasicChrgAmt * surCharge : totalSurChargeAmt = 0;
     document.querySelector('output.surcharge').innerText = (`$${totalSurChargeAmt.toFixed(2)}`);
-
-
     ///for total due
     let totalDue = totalBasicChrgAmt + totalOptChrgAmt + totalSurChargeAmt;
     document.querySelector('output.total').innerText = (`$${totalDue.toFixed(2)}`);
